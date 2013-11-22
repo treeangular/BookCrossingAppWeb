d926ac1123deff2def734f51b1438ee711680563
Marc Canals Giraut <dirkom@gmail.com>
2013-11-22 11:23:47 +0100

    Set production Parse Id

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,8 +18,12 @@ BookCrossingAppWeb.config(['$routeProvider','$httpProvider', function ($routePro
 
 function loadParse()
 {
-    var PARSE_APP_ID = "MyXalB83PFKV15BOPSO2lKBBzkYeyLKGNYsNI5DS";
-    var PARSE_JS_ID = "7pNuZLzLEArqUc2BlQNmDgD5HMVL4l3G9ZIKP3Qr";
+    /* Test
+     var PARSE_APP_ID = "MyXalB83PFKV15BOPSO2lKBBzkYeyLKGNYsNI5DS";
+     var PARSE_JS_ID = "7pNuZLzLEArqUc2BlQNmDgD5HMVL4l3G9ZIKP3Qr";*/
+    //Production
+    var PARSE_APP_ID = "j7SSabUR9BT5xXM0r466Wthn7FSEDdIT1RcjqWnP";
+    var PARSE_JS_ID = "tHuZdyxD04OY6S4ejMlUeqPrIoKoKJi6TJItcmlN";
 
     Parse.initialize(PARSE_APP_ID, PARSE_JS_ID);
 }
